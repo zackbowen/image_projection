72f2ee7408a756b95a6e931de8095c1d4883bb49
zackbowen <zab76@pitt.edu>
2025-12-09 22:31:35 -0500

    added SIFT

--- src/homography/src/homography.cpp
+++ src/homography/src/homography.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 #include <rclcpp/rclcpp.hpp>
 
-#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 // Running the code
 // colcon build
@@ -17,12 +20,13 @@ int main()
     std::cout << std::filesystem::current_path() <<std::endl;
 
     // read in images
+    // TODO improve
     std::vector<cv::Mat> images;
-    images.push_back(cv::imread("homography/images/im0.jpg", cv::IMREAD_COLOR));
-    images.push_back(cv::imread("homography/images/im1.jpg", cv::IMREAD_COLOR));
-    images.push_back(cv::imread("homography/images/im2.jpg", cv::IMREAD_COLOR));
-    images.push_back(cv::imread("homography/images/im3.jpg", cv::IMREAD_COLOR));
-    images.push_back(cv::imread("homography/images/im4.jpg", cv::IMREAD_COLOR));
+    images.push_back(cv::imread("src/homography/images/im0.jpg", cv::IMREAD_COLOR));
+    images.push_back(cv::imread("src/homography/images/im1.jpg", cv::IMREAD_COLOR));
+    images.push_back(cv::imread("src/homography/images/im2.jpg", cv::IMREAD_COLOR));
+    images.push_back(cv::imread("src/homography/images/im3.jpg", cv::IMREAD_COLOR));
+    images.push_back(cv::imread("src/homography/images/im4.jpg", cv::IMREAD_COLOR));
 
 
     for(size_t i = 0; i < images.size(); ++i)
@@ -32,10 +36,25 @@ int main()
             std::cout << "Could not read image " << i << std::endl;
             return 1;
         }
-        
+
+        std::cout << "Image " << i << std::endl;
+
+        // SIFT feature detection
+        cv::Ptr<cv::SiftFeatureDetector> detector = cv::SiftFeatureDetector::create();
+        std::vector<cv::KeyPoint> keypoints;
+        detector->detect(images[i], keypoints);
+
     }
 
-    // SIFT feature detection
+    // view
+    // std::string window_name = "main";
+
+    // cv::namedWindow(window_name);
+
+    // cv::Mat output;
+    // cv::drawKeypoints(images[0], keypoints, output);
+    // cv::imshow(window_name, output);
+    // cv::waitKey(0);
 
     
 
